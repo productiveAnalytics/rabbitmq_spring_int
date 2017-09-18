043bd9a3f1057d74c4462f278fb8429ca0c4b5c4
LChawathe <lchawathe@productiveAnalytics.com>
2017-09-18 02:59:29 -0500

    1. Defined binding b/w Direct Exchange and Queue. 2. Sometimes either Queue or DirectExchange takes time to defined as Bean. (That causes : "Requested bean is currently in creation: Is there an unresolvable circular reference?"). So used @PostConstuct to allow Spring to fully declare required Queue & Exchange beans for the Binding.

--- src/main/java/com/productiveanalytics/rabbitmq_spring_int/config/RabbitMQBootstrap.java
+++ src/main/java/com/productiveanalytics/rabbitmq_spring_int/config/RabbitMQBootstrap.java
@@ -2,11 +2,14 @@ package com.productiveanalytics.rabbitmq_spring_int.config;
 
 import java.util.Properties;
 
+import javax.annotation.PostConstruct;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.Queue;
@@ -18,6 +21,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 
 import org.springframework.util.Assert;
@@ -51,19 +55,18 @@ public class RabbitMQBootstrap implements InitializingBean
 	@Autowired
 	@Qualifier("messageListenerContainer")
 	private SimpleMessageListenerContainer messageListenerContainer;
+		
+	@Autowired
+	@Qualifier("myQueue")
+	private Queue myQueue;
 	
-	@Bean
-	public Exchange myDirectExchange() {
-		String exchangeName	= rabbitmqProps.getProperty(RabbitMQSpringConstants.PROP_EXCHANGE_NAME);
-		
-		System.out.println("Establishing Exchange: "+ exchangeName);
-		
-		Exchange xchng = new DirectExchange(exchangeName, durable, autoDelete);
-		rabbitmqAdmin.declareExchange(xchng);
-		
-		return xchng;
-	}
-	
+	@Autowired
+	@Qualifier("myDirectExchange")
+	private DirectExchange myDirectExchange;
+
+	/**
+	 * Declare the Queue
+	 */
 	@Bean
     public Queue myQueue() {
 
@@ -71,13 +74,52 @@ public class RabbitMQBootstrap implements InitializingBean
 
 		String queueName	= rabbitmqProps.getProperty(RabbitMQSpringConstants.PROP_QUEUE_NAME);
 		
-		System.out.println("Establishing Q: "+ queueName);
+		System.err.println("Establishing Queue: *********"+ queueName +"*********");
 		
 		Queue newQ = new Queue(queueName, durable, exclusive, autoDelete);
 		rabbitmqAdmin.declareQueue(newQ);
 		
         return newQ;
     }
+	
+	/**
+	 * Declare the Exchange
+	 */
+	@Bean
+	public Exchange myDirectExchange() {
+		String exchangeName	= rabbitmqProps.getProperty(RabbitMQSpringConstants.PROP_EXCHANGE_NAME);
+		
+		System.err.println("Establishing Exchange: *********"+ exchangeName +"*********");
+		
+		Exchange xchng = new DirectExchange(exchangeName, durable, autoDelete);
+		rabbitmqAdmin.declareExchange(xchng);
+		
+		return xchng;
+	}
+	
+	/**
+	 * 
+	 * This is very important to create binding only after let Spring declare Queue & Exchange.
+	 * This will avoid issue "Requested bean is currently in creation: Is there an unresolvable circular reference?"
+	 * 
+	 */
+	@PostConstruct
+	public Binding myDirectExchangeToQueueBinding()
+	{
+		// Routing Key has been kept same as Queue Name
+		String routingKey	= rabbitmqProps.getProperty(RabbitMQSpringConstants.PROP_QUEUE_NAME);
+		
+		Assert.isInstanceOf(DirectExchange.class, myDirectExchange, "Check the Exchange type. Must be Direct exchange fo this binding");
+		
+		System.err.println("Bound (Direct) Exchange: "+ myDirectExchange.getName() + " to Queue: "+ myQueue.getName());
+		
+		Binding routingKeyBasedBinding = BindingBuilder.bind(this.myQueue)
+													   .to(this.myDirectExchange)
+													   .with(routingKey /* designed to be same as QueueName */);
+		rabbitmqAdmin.declareBinding(routingKeyBasedBinding);
+		
+		return routingKeyBasedBinding;
+	}
 
 	public void afterPropertiesSet() throws Exception {
 		Assert.notNull(messageListenerContainer, "RabbitMQ/Spring Integration message listener container is null. FATAL Error!");
@@ -85,6 +127,7 @@ public class RabbitMQBootstrap implements InitializingBean
 		boolean startStatus = RabbitMQUtility.startMessageListenerContainer(messageListenerContainer);
 		if (startStatus) {
 			logger.debug("[RabbitMQBootstrap] RabbitMQ/Spring Integration message listener container is running");
+			System.err.println("[RabbitMQBootstrap] RabbitMQ/Spring Integration message listener container is running");
 		}
 	}
 }
